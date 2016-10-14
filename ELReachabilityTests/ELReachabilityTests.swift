37be9aa081d393037ccc7a63ea95e6b00909db00
Alex Johnson <ajohnson@walmartlabs.com>
2016-10-14 12:04:32 -0700

    Add NetworkConnection enumeration

--- ELReachabilityTests/ELReachabilityTests.swift
+++ ELReachabilityTests/ELReachabilityTests.swift
@@ -7,10 +7,17 @@
 //
 
 import XCTest
+import SystemConfiguration
 @testable import ELReachability
 
 class ELReachabilityTests: XCTestCase {
-    
+    // Examples of various "real world" flag combinations.
+    let offlineFlags: SCNetworkReachabilityFlags = []
+    let notConnectedFlags: SCNetworkReachabilityFlags = [.Reachable, .ConnectionRequired]
+    let transientFlags: SCNetworkReachabilityFlags = [.Reachable, .TransientConnection]
+    let cellularFlags: SCNetworkReachabilityFlags = [.Reachable, .IsWWAN]
+    let wifiFlags: SCNetworkReachabilityFlags = [.Reachable]
+
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -30,12 +37,53 @@ class ELReachabilityTests: XCTestCase {
             XCTAssert(false, "Failed")
         }
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
+
+    // MARK: NetworkConnection Tests
+
+    func testConnectionWhenNotReachable() {
+        XCTAssertNil(NetworkConnection(flags: offlineFlags), "Connection should be nil when offline")
+        XCTAssertNil(NetworkConnection(flags: notConnectedFlags), "Connection should be nil when not connected")
+        XCTAssertNil(NetworkConnection(flags: transientFlags), "Connection should be nil when transient")
     }
-    
+
+    func testConnectionWhenReachable() {
+        XCTAssertEqual(NetworkConnection(flags: cellularFlags), .cellular, "Connection should be `cellular` when connected via Cellular")
+        XCTAssertEqual(NetworkConnection(flags: wifiFlags), .wifi, "Connection should be `wifi` when connected via WiFi")
+    }
+
+    // MARK: NetworkStatusInterpreter Tests
+
+    func testInterpreterWhenNotReachable() {
+        let offline = NetworkStatusInterpreter(flags: offlineFlags)
+        XCTAssertFalse(offline.isReachable, "Status should be not-reachable when offline")
+        XCTAssertNil(offline.connection, "Connection should be nil when offline")
+        XCTAssertFalse(offline.isCellular, "`isCellular` should be false when offline")
+        XCTAssertFalse(offline.isWiFi, "`isWiFi` should be false when offline")
+
+        let notConnected = NetworkStatusInterpreter(flags: notConnectedFlags)
+        XCTAssertFalse(notConnected.isReachable, "Status should be not-reachable when not connected")
+        XCTAssertNil(notConnected.connection, "Connection should be nil when not connected")
+        XCTAssertFalse(notConnected.isCellular, "`isCellular` should be false when not connected")
+        XCTAssertFalse(notConnected.isWiFi, "`isWiFi` should be false when not connected")
+
+        let transient = NetworkStatusInterpreter(flags: transientFlags)
+        XCTAssertFalse(transient.isReachable, "Status should be not-reachable when transient")
+        XCTAssertNil(transient.connection, "Connection should be nil when transient")
+        XCTAssertFalse(transient.isCellular, "`isCellular` should be false when transient")
+        XCTAssertFalse(transient.isWiFi, "`isWiFi` should be false when transient")
+    }
+
+    func testInterpreterWhenReachable() {
+        let cellular = NetworkStatusInterpreter(flags: cellularFlags)
+        XCTAssertTrue(cellular.isReachable, "Status should be reachable when connected via Cellular")
+        XCTAssertEqual(cellular.connection, .cellular, "Connection should be `cellular` when connected via Cellular")
+        XCTAssertTrue(cellular.isCellular, "`isCellular` should be true when connected via Cellular")
+        XCTAssertFalse(cellular.isWiFi, "`isWiFi` should be false when connected via Cellular")
+
+        let wifi = NetworkStatusInterpreter(flags: wifiFlags)
+        XCTAssertTrue(wifi.isReachable, "Status should be reachable when connected via WiFi")
+        XCTAssertEqual(wifi.connection, .wifi, "Connection should be `wifi` when connected via WiFi")
+        XCTAssertTrue(wifi.isWiFi, "`isWiFi` should true when connected via WiFi")
+        XCTAssertFalse(wifi.isCellular, "`isCellular` should be false when connected via WiFi")
+    }
 }
