743240e9bf201e57f3e3678ea6b91546e5e0a4b6
John Regner <regnerjr@gmail.com>
2016-12-09 16:02:02 -0800

    Converted the unit test target to Swift3

--- ELReachabilityTests/ELReachabilityTests.swift
+++ ELReachabilityTests/ELReachabilityTests.swift
@@ -22,14 +22,14 @@ class ELReachabilityTests: XCTestCase {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-    
+
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
+
     // MARK: Tests
-    
+
     func testSynchronousReachableInternet() {
         if let theInternets = NetworkStatus.networkStatusForInternetConnection() {
             XCTAssertTrue(theInternets.isReachable())
